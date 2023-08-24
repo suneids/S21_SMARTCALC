516bf9137fe581fcc671508598b361a250761047
suneids <68694104+suneids@users.noreply.github.com>
2023-08-25 00:09:03 +0300

    add something

--- gitgo.sh
+++ gitgo.sh
@@ -1,10 +1,11 @@
 git status
-echo "Перейти к add & commit? (y/n)\n"
-if [[ "$1" == "y" ]]; then
+echo "Перейти к add & commit? (y/n)"
+read -r answer
+if [[ $answer == "y" ]]; then
     git add .
-    echo "Введите сообщение для коммита\n"
-    git commit -m "$*"
-   
+    echo "Введите сообщение для коммита"
+    read -r message
+    git commit -m "$message"
 else
     echo "Finished"
 fi
