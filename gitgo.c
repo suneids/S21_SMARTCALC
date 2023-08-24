00f0075cab97b308ca94bba1d47cbabe82b84813
suneids <68694104+suneids@users.noreply.github.com>
2023-08-24 23:56:24 +0300

    n

--- gitgo.c
+++ gitgo.c
@@ -1,20 +1,83 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #define status "git status"
 #define add "git add ."
-#define commit "git commit -m"
+#define commit "git commit -m \""
 #define push "git push"
+int git_status();
+int git_add();
+int git_commit();
+int git_push();
 
 int main() {
+	git_status();
+	if (!git_add())
+		return 0;
+	git_commit();
+	git_push();
+	return 0;
+}
+
+
+int git_status() {
 	system(status);
-	printf("\ny/n\n");
+}
+
+
+int git_add() {
+	printf("Continue(add, commit)?\n y/n\n");
 	char answer;
 	scanf("%c", &answer);
 	if (answer != 'y')
-		return 0;
+		return 1;
 	system(add);
+}
+
+
+int git_commit() {
+	printf("¬ведите комментарий\n");
+	char* comment = malloc(128 * sizeof(char));
+	char message[200] = { '\0' };
+	char next_letter;
+	int i = 0;
+	do {
+		scanf("%c", &next_letter);
+		if (next_letter != '\n') {
+			comment[i++] = next_letter;
+		}
+
+	} while (next_letter != '\n');
+	comment[i] = '\0';
+	strcat(message, commit);
+	strcat(message, comment);
+	strcat(message, "\"");
+	system(message);
+	free(comment);
+	return 0;
+}
+
+
+int git_push() {
+	printf("Push?\n y/n\n");
+	char answer;
+	scanf("%c", &answer);
+	if (answer != 'y')
+		return 1;
 	printf("¬ведите название ветки\n");
+	char* branch = malloc(128 * sizeof(char));
+	char message[200] = { '\0' }, next_letter;
+	int i = 0;
+	do {
+		scanf("%c", &next_letter);
+		if (next_letter != '\n') {
+			branch[i++] = next_letter;
+		}
 
-	system();
-
+	} while (next_letter != '\n');
+	strcat(message, push);
+	strcat(message, branch);
+	system(message);
+	free(branch);
 	return 0;
 }
