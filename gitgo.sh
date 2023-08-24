52e151db0c722b773db13f4820915aed490625ab
suneids <68694104+suneids@users.noreply.github.com>
2023-08-25 00:13:47 +0300

    Add something

--- gitgo.sh
+++ gitgo.sh
@@ -6,13 +6,13 @@ if [[ $answer == "y" ]]; then
     echo "Введите сообщение для коммита"
     read -r message
     git commit -m "$message"
-    read -r answer
     echo "Перейти к push? (y/n)"
+    read -r answer
     if [[ $answer == "y" ]]; then
         echo "Введите ветку"
 	read -r branch
         git push $branch
-    fi	
+    fi
 else
     echo "Finished"
 fi
