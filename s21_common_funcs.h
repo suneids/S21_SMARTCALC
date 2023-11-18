654f7b33c161c24e146e9bdb4c48b2ed9342e436
suneids <68694104+suneids@users.noreply.github.com>
2023-11-18 16:39:27 +0300

    completed

--- s21_common_funcs.h
+++
@@ -1,7 +0,0 @@
-#ifndef S21_COMMON_FUNCS_H
-#define S21_COMMON_FUNCS_H
-int s21_is_oper(char symbol);
-int s21_is_oper_or_func(char *lexem);
-#endif
-
-
--- s21_lexem_parse.h
+++
@@ -1,7 +0,0 @@
-#ifndef S21_LEXEM_PARSE_H
-#define S21_LEXEM_PARSE_H
-char **s21_get_lexems(char *input_string, int *lexems_count);
-char *s21_get_next_lexem(char *input_string, int *start_index);
-void s21_destroy_lexems(char **lexems, int size);
-void s21_remove_spaces(char *input);
-#endif
